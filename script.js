a92daf85b92a1978f7c96681e313913d7723ffb6
Rin Ensaka <138004714+lyxgy@users.noreply.github.com>
2025-02-06 22:07:10 +0800

    第二次提交网站

---
+++ script.js
@@ -0,0 +1,10 @@
+// 平滑滚动效果
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
